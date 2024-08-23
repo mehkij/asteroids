f0dd2c461f995fbc185aaf42ffdb888b59daf9e9
Mehki Joseph <disismehki@gmail.com>
2024-08-23 04:04:18 -0400

    Create main function

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print("Screen Width:", SCREEN_WIDTH)
+    print("Screen Height:", SCREEN_HEIGHT)
+
+if __name__ == "__main__":
+    main()
