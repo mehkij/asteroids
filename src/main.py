663e96a5d506a42d13d8ca67fd91bd17a272d6a5
Mehki Joseph <disismehki@gmail.com>
2024-08-23 04:33:58 -0400

    Create clock

--- src/main.py
+++ src/main.py
@@ -9,6 +9,8 @@ def main():
     print("Screen height:", SCREEN_HEIGHT)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while pygame.init():
         for event in pygame.event.get():
@@ -17,6 +19,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        # Limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
