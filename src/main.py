9249f94111de181430f7eb8007afc613377b173a
Mehki Joseph <disismehki@gmail.com>
2024-08-23 04:17:31 -0400

    Init pygame and create event loop in main

--- src/main.py
+++ src/main.py
@@ -2,9 +2,21 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    
     print("Starting asteroids!")
-    print("Screen Width:", SCREEN_WIDTH)
-    print("Screen Height:", SCREEN_HEIGHT)
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while pygame.init():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
