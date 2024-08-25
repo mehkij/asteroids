bf48451951e0d39439bce3a74c53ac4dcd82644c
Mehki Joseph <disismehki@gmail.com>
2024-08-25 03:33:56 -0400

    Create new group for Shots class

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -24,6 +25,9 @@ def main():
 
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
+
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
 
