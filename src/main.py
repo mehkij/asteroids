d893835b9ad4f93102ad6773b7d16f5628cdc6fb
Mehki Joseph <disismehki@gmail.com>
2024-08-25 00:48:11 -0400

    Create new groups and container

--- src/main.py
+++ src/main.py
@@ -13,6 +13,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
 
     while pygame.init():
@@ -20,10 +24,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
         
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
 
         # Limit framerate to 60 FPS
